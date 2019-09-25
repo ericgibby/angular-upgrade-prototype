ac18563c050209a322bb2c6f583755c33e6e0473
Eric Gibby <ericgibby@users.noreply.github.com>
2019-09-25 13:53:17 -0600

    Moved DataTableController into component

--- src/components/data-table/data-table.component.js
+++ src/components/data-table/data-table.component.js
@@ -1,11 +1,39 @@
-angular.module('components.dataTable').component('dataTable', {
+angular.module('components.dataTable').component('myDataTable', {
 	bindings: {
 		columns: '<',
-		controller: 'DataTableController',
-		tableData: '<',
-		templateUrl: 'components/data-table/data-table.html'
-	}
+		tableData: '<'
+	},
+	controller: DataTableController,
+	templateUrl: 'components/data-table/data-table.html'
 });
 
-DataTableComponent.$inject = [];
-function DataTableComponent() {}
+DataTableController.$inject = [];
+function DataTableController() {
+	const $ctrl = this;
+
+	// Model
+	$ctrl.columnMap = {};
+	$ctrl.columnKeys = [];
+
+	// Public functions
+	$ctrl.$onInit = $onInit;
+
+	////////////////////
+
+	function $onInit() {
+		if ($ctrl.columns) {
+			if (Array.isArray($ctrl.columns)) {
+				$ctrl.columnMap = $ctrl.columns.reduce((obj, key) => ({ ...obj, [key]: key }), {});
+			} else {
+				$ctrl.columnMap = { ...$ctrl.columns };
+			}
+		} else {
+			$ctrl.columnMap = Object.keys(($ctrl.tableData && $ctrl.tableData[0]) || []).reduce(
+				(obj, key) => ({ ...obj, [key]: key }),
+				{}
+			);
+		}
+
+		$ctrl.columnKeys = Object.keys($ctrl.columnMap);
+	}
+}
--- src/components/data-table/data-table.component.spec.js
+++ src/components/data-table/data-table.component.spec.js
@@ -1,43 +1,47 @@
 describe('DataTableController', () => {
-	let $controller;
-	let $ctrl;
+	let $componentController;
 
 	beforeEach(() => {
 		module('components.dataTable');
 		inject($injector => {
-			$controller = $injector.get('$controller');
+			$componentController = $injector.get('$componentController');
 		});
-		$ctrl = $controller('DataTableController');
 	});
 
 	afterEach(() => {
-		$controller = undefined;
+		$componentController = undefined;
 	});
 
 	it('initializes without error', () => {
-		expect($ctrl).toBeDefined();
+		const ctrl = $componentController('dataTable');
+		expect(ctrl).toBeDefined();
 	});
 
 	describe('.$onInit()', () => {
 		it('populates `columnMap` object from `columns` object if specified', () => {
-			$ctrl.columns = { col1: 'col1', col2: 'col2', col3: 'col3' };
-			$ctrl.$onInit();
-			expect($ctrl.columnMap).toEqual($ctrl.columns);
+			const bindings = { columns: { col1: 'col1', col2: 'col2', col3: 'col3' } };
+			const ctrl = $componentController('dataTable', undefined, bindings);
+			ctrl.$onInit();
+			expect(ctrl.columnMap).toEqual(bindings.columns);
 		});
 
 		it('populates `columnMap` from `columns` specified as array', () => {
-			$ctrl.columns = ['col1', 'col2', 'col3'];
-			$ctrl.$onInit();
-			expect($ctrl.columnMap).toEqual({ col1: 'col1', col2: 'col2', col3: 'col3' });
+			const bindings = { columns: ['col1', 'col2', 'col3'] };
+			const ctrl = $componentController('dataTable', undefined, bindings);
+			ctrl.$onInit();
+			expect(ctrl.columnMap).toEqual({ col1: 'col1', col2: 'col2', col3: 'col3' });
 		});
 
 		it('populates `columnMap` object from `tableData` if `columns` not specified', () => {
-			$ctrl.tableData = [
-				{ col1: 'Value 1', col2: 'Value 2', col3: 'Value 3' },
-				{ col1: 'Value 4', col2: 'Value 5', col3: 'Value 6' }
-			];
-			$ctrl.$onInit();
-			expect($ctrl.columnMap).toEqual({ col1: 'col1', col2: 'col2', col3: 'col3' });
+			const bindings = {
+				tableData: [
+					{ col1: 'Value 1', col2: 'Value 2', col3: 'Value 3' },
+					{ col1: 'Value 4', col2: 'Value 5', col3: 'Value 6' }
+				]
+			};
+			const ctrl = $componentController('dataTable', undefined, bindings);
+			ctrl.$onInit();
+			expect(ctrl.columnMap).toEqual({ col1: 'col1', col2: 'col2', col3: 'col3' });
 		});
 	});
 });

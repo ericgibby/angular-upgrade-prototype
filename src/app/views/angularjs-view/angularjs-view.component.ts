28fa10305a8bbf221b6fa336521faadd886f1dcb
Eric Gibby <ericgibby@users.noreply.github.com>
2019-10-08 10:16:25 -0600

    Updated template

--- src/app/views/angularjs-view/angularjs-view.component.ts
+++ src/app/views/angularjs-view/angularjs-view.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
 	selector: 'app-angularjs-view',
 	templateUrl: './angularjs-view.component.html',
-	styleUrls: ['./angularjs-view.component.scss']
+	styleUrls: ['./angularjs-view.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class AngularjsViewComponent implements OnInit {
 	constructor() {}

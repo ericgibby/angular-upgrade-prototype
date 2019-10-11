395425d1bc042aaa1bdcb016d4c7d985da9ef03d
Eric Gibby <ericgibby@users.noreply.github.com>
2019-10-11 13:27:07 -0600

    Load empty template for 'app.table' state

--- src/views/table/table.config.js
+++ src/views/table/table.config.js
@@ -4,9 +4,6 @@ config.$inject = ['$stateProvider'];
 function config($stateProvider) {
 	$stateProvider.state('app.table', {
 		url: 'table',
-		controller: 'TableController',
-		controllerAs: '$ctrl',
-		bindToController: true,
-		templateUrl: 'views/table/table.html'
+		template: ''
 	});
 }

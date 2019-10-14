934644271f6934e46f1441e296aa4310bcd89d18
Eric Gibby <ericgibby@users.noreply.github.com>
2019-10-14 10:39:29 -0600

    Register modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UpgradeModule } from '@angular/upgrade/static';
+import { AngularFoundationModule } from '@ericgibby/angular-foundation';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DowngradeModule } from './downgrade/downgrade.module';
-import { apiServiceProvider } from './services/api.service';
 import { TableComponent } from './views/table/table.component';
 
 @NgModule({
 	bootstrap: [AppComponent],
 	declarations: [AppComponent, TableComponent],
-	imports: [BrowserModule, UpgradeModule, DowngradeModule, AppRoutingModule],
-	providers: [apiServiceProvider],
-	schemas: [CUSTOM_ELEMENTS_SCHEMA]
+	imports: [BrowserModule, UpgradeModule, DowngradeModule, AppRoutingModule, AngularFoundationModule]
 })
 export class AppModule {
 	constructor(public upgrade: UpgradeModule) {}

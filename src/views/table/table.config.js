e18dc2375752c759b142cbc4b5e1f353a3b8a95c
Eric Gibby <ericgibby@users.noreply.github.com>
2019-11-26 14:22:42 -0700

    Added AngularJS view to demonstrate downgraded Angular component

--- src/views/table/table.config.js
+++ src/views/table/table.config.js
@@ -2,8 +2,16 @@ angular.module('app.table').config(config);
 
 config.$inject = ['$stateProvider'];
 function config($stateProvider) {
-	$stateProvider.state('app.table', {
-		url: 'table',
-		template: ''
-	});
+	$stateProvider
+		.state('app.table', {
+			url: 'table',
+			template: ''
+		})
+		.state('app.oldTable', {
+			url: 'old-table',
+			controller: 'TableController',
+			bindToController: true,
+			controllerAs: '$ctrl',
+			templateUrl: 'views/table/table.html'
+		});
 }

d38a036bb91b6c78739c01abaca81d095db080e2
Eric Gibby <ericgibby@users.noreply.github.com>
2020-02-05 10:44:26 -0700

    Wired up AngularJS ApiService to use new getTableData function

--- src/services/api/api.service.js
+++ src/services/api/api.service.js
@@ -1,10 +1,9 @@
+import { getTableData } from '../../app/services/api';
+
 window.angular.module('services.api').service('ApiService', ApiService);
 
-ApiService.$inject = ['$http', 'URLS'];
-function ApiService($http, URLS) {
-	this.getTableData = getTableData;
-
-	function getTableData() {
-		return $http.get(URLS.api).then(({ data }) => data.users);
-	}
+ApiService.$inject = ['$timeout'];
+function ApiService($timeout) {
+	// Wrap in $timeout to keep it in the digest cycle
+	this.getTableData = () => $timeout(() => getTableData().toPromise());
 }

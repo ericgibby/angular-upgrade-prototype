48819401eb8f26df22b93ca503ebd3afad8147ae
Eric Gibby <ericgibby@users.noreply.github.com>
2020-02-05 14:40:24 -0700

    Listen for location changes inside AngularJS so react-router can be updated

--- src/app.config.js
+++ src/app.config.js
@@ -1,4 +1,9 @@
-window.angular.module('app').config(config);
+import { history } from './app/services/history';
+
+window.angular
+	.module('app')
+	.config(config)
+	.run(run);
 
 config.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
 function config($locationProvider, $stateProvider, $urlRouterProvider) {
@@ -13,4 +18,15 @@ function config($locationProvider, $stateProvider, $urlRouterProvider) {
 	if (process.env.NODE_ENV !== 'karma') {
 		$urlRouterProvider.otherwise('/home');
 	}
+}
+
+run.$inject = ['$rootScope', '$urlMatcherFactory', '$urlRouter'];
+function run($rootScope) {
+	// Need to monitor route changes inside AngularJS so react-router can be updated
+	$rootScope.$on('$locationChangeSuccess', (event, newUrl, oldUrl) => {
+		if (oldUrl !== newUrl) {
+			const url = newUrl.replace(/^(http[s]?:\/\/)?[^/]+/, '');
+			history.replace(url);
+		}
+	});
 }

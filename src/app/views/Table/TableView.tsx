d32914fdc67714e91b6349988c4978f5fc627352
Eric Gibby <ericgibby@users.noreply.github.com>
2020-02-06 10:54:10 -0700

    Added AngularJS grid to TableView component

--- src/app/views/Table/TableView.tsx
+++ src/app/views/Table/TableView.tsx
@@ -1,5 +1,8 @@
+// eslint-disable-next-line import/no-webpack-loader-syntax
 import React, { useEffect, useState } from 'react';
 import { catchError, tap } from 'rxjs/operators';
+import '../../../components/grid/grid.component';
+import AngularJs from '../../components/AngularJs/AngularJs';
 import Table from '../../components/Table/Table';
 import { getTableData } from '../../services/api';
 
@@ -19,15 +22,32 @@ export default function TableView() {
 		return () => sub.unsubscribe();
 	}, []);
 
+	const bindings = {
+		columns: { firstName: 'First Name', lastName: 'Last Name', email: 'Email', dateCreated: 'Created' },
+		data
+	};
+
 	return (
 		<React.Fragment>
 			<h2>
 				The Table View <small>React</small>
 			</h2>
 			<p>This is a page that displays a table with useless data.</p>
+			<h3>
+				React Table <small>React</small>
+			</h3>
 			<Table
 				columns={{ firstName: 'First Name', lastName: 'Last Name', email: 'Email', dateCreated: 'Created' }}
 				data={data}
+			/>
+			<h3>
+				AngularJS Grid <small>AngularJS</small>
+			</h3>
+			<AngularJs
+				bindings={bindings}
+				module="app.grid"
+				dependencies={['components.grid']}
+				template={`<grid columns="columns" data="data"></grid>`}
 			/>
 		</React.Fragment>
 	);
